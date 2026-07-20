166fcf393460d22ca353597ca349ee08fa1c630d
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-20 22:53:16 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -86,10 +86,6 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-# Soong
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
 PRODUCT_PACKAGES += \
     create_pl_dev \
     create_pl_dev.recovery
