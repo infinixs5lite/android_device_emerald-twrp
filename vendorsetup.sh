04c51b4f5d40fac2b726e1d24c6cac678a275ac7
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-20 22:19:33 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,7 +20,8 @@
 #
 
 export LC_ALL="C.UTF-8"
-export FOX_VARIANT="K5.10"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_VARIANT="K6.10"
 export FOX_MAINTAINER_PATCH_VERSION=$(date +"%Y%m%d")
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
