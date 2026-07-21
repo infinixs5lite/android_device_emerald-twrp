92581aba60066eeef31943c9c97ada005c6b6ea2
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-21 19:46:40 +0100

    Update twrp_emerald.mk

--- twrp_emerald.mk
+++ twrp_emerald.mk
@@ -18,11 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration.
 $(call inherit-product, vendor/twrp/config/common.mk)
