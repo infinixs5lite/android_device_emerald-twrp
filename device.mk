cdafdd0e095bd571c64a34e2145290ad476e1cec
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-25 23:59:00 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,9 +25,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Configure virtual_ab_ota compression_with_xor.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
-
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
