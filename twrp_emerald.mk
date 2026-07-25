03c2cbfa0dedfa8cefd81bfb91e8f28c9afebda1
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-25 23:17:30 +0100

    Update twrp_emerald.mk

--- twrp_emerald.mk
+++ twrp_emerald.mk
@@ -17,7 +17,7 @@
 DEVICE_PATH := device/xiaomi/emerald 
 
 # Inherit from device.mk configuration
-$(call inherit-product, device/xiaomi/emerald/device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 ## Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := emerald 
