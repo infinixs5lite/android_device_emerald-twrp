b9b1f4cf7981ccc47401b314458f5ad40814d1e3
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-13 08:38:39 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,6 +1,8 @@
 # Copyright (C) 2024 The Android Open Source Project
 # SPDX-License-Identifier: Apache-2.0
 
+DEVICE_PATH := device/xiaomi/emerald
+
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
