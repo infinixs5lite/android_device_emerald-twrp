20082dd27f2baa901d2491e1b280c574f21b2c25
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-13 08:52:50 +0100

    Update twrp_emerald.mk

--- twrp_emerald.mk
+++ twrp_emerald.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2025 The TWRP Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,18 @@
 # limitations under the License.
 #
 
-# Define hardware platform
-PRODUCT_RELEASE_NAME := emerald 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Device path for OEM device tree
-DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
-
-# Inherit some common TWRP stuff.
+# Inherit from our custom product configuration.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device specific configs.
+$(call inherit-product, device/xiaomi/emerald/device.mk)
 
 ## Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := emerald 
