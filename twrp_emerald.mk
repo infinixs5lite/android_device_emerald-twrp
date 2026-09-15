41ed311841ff3f3401b525798e29d5eb58466861
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-15 19:00:08 +0100

    Update twrp_emerald.mk

--- twrp_emerald.mk
+++ twrp_emerald.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2022 The TWRP Open Source Project
+# Copyright 2024 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,22 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#
+# Only the below variable(s) need to be changed!
+#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/emerald/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-# Device specific configs.
-$(call inherit-product, device/xiaomi/emerald/device.mk)
 
 ## Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := emerald 
