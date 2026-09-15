ab114e343272031352918eb8a935a0e544c7f103
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-15 19:12:17 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,89 +1,88 @@
-#!/bin/bash
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2026 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
+#!/usr/bin/env bash
+export LC_ALL="C"
+export TARGET_ARCH="arm64"
 
-export LC_ALL="C.UTF-8"
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_VARIANT="K6.10"
-export FOX_MAINTAINER_PATCH_VERSION=$(date +"%Y%m%d")
+# Some about us
+export FOX_VERSION="R12.1_0"
+export OF_MAINTAINER="nino"
+
+# Build environment stuffs
+export FOX_BUILD_DEVICE="emerald"
+export FOX_VARIANT="Test"
+export FOX_BUILD_TYPE="Unofficial"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export TARGET_DEVICE_ALT="gold"
+
+# Magiskboot
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export FOX_PATCH_VBMETA_FLAG=1
+
+# data stuff
+export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+export OF_NO_RELOAD_AFTER_DECRYPTION=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_USE_GREEN_LED=0
+export OF_FLASHLIGHT_ENABLE=0
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+
+# Keymaster
+export OF_DEFAULT_KEYMASTER_VERSION="4.1"
+
+# We have a/b partitions
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
-export FOX_ENABLE_APP_MANAGER=1
+
+# Vendor Boot recovery
+export FOX_VENDOR_BOOT_RECOVERY=1
+export FOX_RECOVERY_VENDOR_BOOT_PARTITION="/dev/block/by-name/vendor_boot"
+
+# Vendor & System
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-export FOX_RECOVERY_VENDOR_BOOT_PARTITION="/dev/block/by-name/vendor_boot"
-export FOX_VENDOR_BOOT_RECOVERY_FULL_REFLASH=1
-export FOX_VENDOR_BOOT_RECOVERY=1
-export FOX_USE_XZ_UTILS=1
-export FOX_USE_UPDATED_MAGISKBOOT=1
-export FOX_TARGET_DEVICES="emerald"
-export TARGET_DEVICE_ALT="emerald"
-export FOX_DISABLE_UPDATEZIP=1
-export FOX_DELETE_AROMAFM=1
-export FOX_DELETE_MAGISK_ADDON=1
-export FOX_DELETE_INITD_ADDON=1
-export FOX_REMOVE_BASH=1
-export FOX_REMOVE_AAPT=1
 
-##########################################
-##### OrangeFox specific build flags #####
-##########################################
-
-# Screen
+# Screen specifications
 export OF_SCREEN_H=2400
-export OF_STATUS_H=100
-export OF_STATUS_INDENT_LEFT=48
-export OF_STATUS_INDENT_RIGHT=48
+export OF_STATUS_H=87
+export OF_STATUS_INDENT_LEFT=85
+export OF_STATUS_INDENT_RIGHT=85
+export OF_HIDE_NOTCH=1
 export OF_CLOCK_POS=1
+export OF_ALLOW_DISABLE_NAVBAR=0
 
-# Maintainer
-export OF_MAINTAINER=nino
+# Device stuff
+export OF_PATCH_AVB20=1
+export OF_DYNAMIC_FULL_SIZE=9126805504
+export OF_UNBIND_SDCARD_F2FS=1
+export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
 
-# OrangeFox settings
-export OF_DEFAULT_KEYMASTER_VERSION=4.1
-export OF_FLASHLIGHT_ENABLE=1
-export OF_NO_REFLASH_CURRENT_ORANGEFOX=1
-export OF_NO_SPLASH_CHANGE=1
-export OF_FORCE_CASEFOLDING=1
+# For some reason this is dumb and necessary
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"
 
-# Recovery additional features
-export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/super;/vendor_boot;"
+
+# OTA & MIUI
+export OF_NO_MIUI_PATCH_WARNING=1
+# export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+# Dont install AROMAFM
+export FOX_DELETE_AROMAFM=1
+
+# Add some extras
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_GREP_BINARY=1
+export FOX_USE_XZ_UTILS=1
 export OF_ENABLE_LPTOOLS=1
+export FOX_ASH_IS_BASH=1
+export FOX_REPLACE_TOOLBOX_GETPROP=1
+export FOX_USE_BASH_SHELL=1
 export OF_USE_LZ4_COMPRESSION=1
-export OF_DISPLAY_FORMAT_FILESYSTEMS_DEBUG_INFO=1
+export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
 
-export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-
-# ----- data format stuff -----
-# ensure that /sdcard is bind-unmounted before f2fs data repair or format
-OF_UNBIND_SDCARD_F2FS=1
-export OF_FORCE_DATA_FORMAT_F2FS=1
-
-# use dmctl to work around problems with formatting the /data partition
-export OF_USE_DMCTL=1
-
-# automatically wipe /metadata after data format
-export OF_WIPE_METADATA_AFTER_DATAFORMAT=1
-
-# avoid MTP issues after data format
-export OF_BIND_MOUNT_SDCARD_ON_FORMAT=1
-
-# don't spam the console with loop errors
-export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
