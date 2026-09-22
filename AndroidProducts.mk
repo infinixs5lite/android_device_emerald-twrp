3a44bfc22d47d6bef18bee0b804ccac04a85fb6b
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-22 15:04:21 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,4 +16,7 @@
 
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_emerald.mk
 
-COMMON_LUNCH_CHOICES := twrp_emerald-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_emerald-user \
+    twrp_emerald-userdebug \
+    twrp_emerald-eng
