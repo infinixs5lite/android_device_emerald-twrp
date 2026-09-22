ab936621fba8c2dcdaf5fdd7ca6a257a14457183
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-09-22 17:12:46 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+DEVICE_PATH := device/xiaomi/emerald
+
 # Configure base.mk
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
